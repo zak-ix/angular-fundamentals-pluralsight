1913848f45dc652e33171d26f382043b5a657972
Muhammad Zakria <muhammad.zakria@leanix.net>
2024-11-26 22:58:00 +0100

    form validation single validator

--- src/app/site-header/site-header.component.ts
+++ src/app/site-header/site-header.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { IUser } from '../user/user.model';
+import { UserService } from '../user/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-site-header',
   templateUrl: './site-header.component.html',
   styleUrls: ['./site-header.component.css'],
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
 })
-export class SiteHeaderComponent {
+export class SiteHeaderComponent implements OnInit {
+
+  showSignOutMenu = signal<boolean>(false)
+  user = signal<IUser | null>(null)
+  private userService = inject(UserService)
 
   constructor() { }
 
+  ngOnInit(): void {
+      this.userService.getUser().subscribe({
+        next: (user) => { this.user.set(user)}
+      })
+  }
+
+  toggleSignOutMenu() {
+    this.showSignOutMenu.update((prev) => !prev)
+  }
+
+  signOut() {
+    this.userService.signOut();
+    this.showSignOutMenu.set(false)
+  }
+ 
 }
--- src/app/user/sign-in/sign-in.component.ts
+++ src/app/user/sign-in/sign-in.component.ts
@@ -3,25 +3,28 @@ import { FormsModule } from '@angular/forms';
 import { IUserCredentials } from '../user.model';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css'],
   standalone: true,
-  imports: [FormsModule]
+  imports: [FormsModule, CommonModule]
 })
 export class SignInComponent {
   credentials = signal<IUserCredentials>({ email: '', password: ''})
+  signInError = signal<boolean>(false)
   private userService = inject(UserService)
   private router = inject(Router)
 
   constructor() { }
 
   signIn() {
+    this.signInError.set(false)
     this.userService.signIn(this.credentials()).subscribe({
-      next: () => this.router.navigate(['/catalog'])
+      next: () => this.router.navigate(['/catalog']),
+      error: () => this.signInError.set(true)
     })
   }
-
 }
